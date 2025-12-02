d6215b10c38fac3ab1d674cfb921e26f11dfc4d7
IacobRuben-Alexandru <iacob.ruben.a@gmail.com>
2025-12-02 17:27:26 +0200

    Add project files.

---
+++ Pacanele/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Pacanele
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(13, 13);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(646, 425);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Trage";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(666, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bani:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(713, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(666, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(663, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Miza: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(704, 77);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(704, 114);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(663, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cicluri:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(666, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Pacanele/Form1.cs
@@ -0,0 +1,256 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace Pacanele
+{
+    
+    public partial class Form1: Form
+    {
+        float eyePosX;
+        bool gameOver = false;
+        float eyePosY;
+        float eyePosZ;
+        int cicluri = 1;
+        float camDepth;
+        int bani = 100;
+        int miza;
+        bool inTragere = false;
+        bool trage;
+        int lastA = 0, lastB = 0, lastC = 0; 
+        bool firstDraw = true;
+        int[] textures = new int[4];
+        Random rnd = new Random();
+        int a, b, c;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            glControl1.Load += Form1_Load;
+            glControl1.Paint += Form1_Paint;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetupValues();
+            GL.Enable(EnableCap.Texture2D);
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+            LoadTextures();
+        }
+
+        private void SetupValues()
+        {
+            camDepth = 1.04f;
+            numericUpDown2.Value = 1;
+        }
+        private void LoadTextures()
+        {
+            GL.GenTextures(textures.Length, textures);
+            LoadTexture(textures[0], "cirese.png");
+            LoadTexture(textures[1], "lamaie.png");
+            LoadTexture(textures[2], "pruna.png");
+            LoadTexture(textures[3], "sapte.png");
+        }
+        private void LoadTexture(int textureId, string filename)
+        {
+            Bitmap bmp = new Bitmap(filename);
+
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                                                    System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                                                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.BindTexture(TextureTarget.Texture2D, textureId);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                          bmp.Width, bmp.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
+                          PixelType.UnsignedByte, data.Scan0);
+
+            bmp.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (float)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (float)TextureMagFilter.Linear);
+        }
+        private void Form1_Paint(object sender, EventArgs e)
+        {
+            glControl1.MakeCurrent();
+            GL.ClearColor(Color.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView((float)camDepth, (float)glControl1.Width / (float)glControl1.Height, 1, 10000);   
+            Matrix4 lookat = Matrix4.LookAt(80, 40, 100, 80, 40, 0, 0, 1, 0);                   
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref perspective);
+            GL.MatrixMode(MatrixMode.Modelview);                                                    
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref lookat);
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height);                                     
+            GL.Enable(EnableCap.DepthTest);                                                               
+            GL.DepthFunc(DepthFunction.Less);
+            //GL.Enable(EnableCap.Texture2D);
+            //GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+            //LoadTextures();
+            if (!firstDraw)
+            {
+                drawSlot(0, 0, lastA);
+                drawSlot(60, 0, lastB);
+                drawSlot(120, 0, lastC);
+            }
+            else
+            {
+                label6.Text = "Distractie placuta";
+                drawSlot(0, 0, 3);
+                drawSlot(60, 0, 3);
+                drawSlot(120, 0, 3);
+            }
+
+            label2.Text = bani.ToString();
+           
+            glControl1.SwapBuffers();
+        }
+        private async Task Tragere()
+        {
+            int tempA, tempB, tempC; 
+            if (bani - miza < 0)
+            {
+                miza = bani;
+                numericUpDown1.Value = miza;
+            }
+            bani = bani - miza;
+            label2.Text = bani.ToString();
+            for (int i = 0; i < cicluri; i++)
+            {
+                tempA = rnd.Next(0, 4);
+                tempB = rnd.Next(0, 4);
+                tempC = rnd.Next(0, 4);
+
+                lastA = tempA;
+                lastB = tempB;
+                lastC = tempC;
+
+                glControl1.Invalidate();
+                // ca sa fie conform cerintei ar trebui a pun await Task.Delay(500) dar e foarte slow;
+                await Task.Delay(500);        
+            }
+            a = rnd.Next(0, 4);
+            b = rnd.Next(0, 4);
+            c = rnd.Next(0, 4);
+            lastA = a;
+            lastB = b;
+            lastC = c;
+            label2.Text = bani.ToString();
+            //drawSlot(0, 0, a);
+            //drawSlot(60, 0, b);
+            //drawSlot(120, 0, c);
+            if (a == b && b == c && c == 0)
+            {
+                bani += miza * 15;
+                label6.Text = "Ati castigat";  
+            }
+            else if (a == b && b == c && c == 1)
+            {
+                bani += miza * 20;
+                label6.Text = "Ati castigat";
+            }
+            else if (a == b && b == c && c == 2)
+            {
+                bani += miza * 10;
+                label6.Text = "Ati castigat";
+            }
+            else if (a == b && b == c && c == 3)
+            {
+                bani += miza * 100;
+                label6.Text = "Ati castigat";
+            }
+            else
+            {
+                label6.Text = "Ati pierdut";
+            }
+            if (bani == 0)
+            {
+                gameOver = true;
+                label6.Text = "Ati pierdut tot";
+            }
+
+            firstDraw = false;
+            label2.Text = bani.ToString();
+            trage = false;
+            inTragere = false;
+            glControl1.Invalidate();
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            miza = (int)numericUpDown1.Value;
+        }
+
+        private void drawSlot(int xStart, int yStart, int textureId)
+        {
+            GL.BindTexture(TextureTarget.Texture2D, textures[textureId]);
+            GL.Begin(PrimitiveType.Quads);
+            GL.TexCoord2(0, 1); GL.Vertex3(xStart, yStart, 0);
+            GL.TexCoord2(1, 1); GL.Vertex3(xStart + 40, yStart, 0);
+            GL.TexCoord2(1, 0); GL.Vertex3(xStart + 40, yStart + 80, 0);
+            GL.TexCoord2(0, 0); GL.Vertex3(xStart, yStart + 80, 0);
+            GL.End();
+            
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            cicluri = (int)numericUpDown2.Value;
+            glControl1.Invalidate();
+        }
+
+        int ok = 0;
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            firstDraw = false;
+            if(!inTragere)
+            {
+                inTragere = true;
+                
+
+                await Tragere();
+                if (gameOver)
+                {
+                    this.BeginInvoke(new Action(() =>
+                    {
+                        DialogResult result = MessageBox.Show(
+                            "Ai pierdut tot! Vrei să încerci din nou?",
+                            "Game Over",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Warning
+                        );
+
+                        if (result == DialogResult.Yes)
+                        {
+                            bani = 100;
+                            label2.Text = bani.ToString();
+                            gameOver = false;
+                            label6.Text = "Distractie placuta";
+                        }
+                        else
+                        {
+                            Application.Exit();
+                        }
+                        gameOver = false;
+                    }));
+                }
+            }
+            
+            glControl1.Invalidate();
+        }
+    }
+}
